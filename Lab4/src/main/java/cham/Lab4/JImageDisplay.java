bce81315e71635727cabc96d73347186f8e276f0
EvilReptile <green0chameleon@yandex.ru>
2020-05-22 13:44:25 +0300

    Правки

--- Lab4/src/main/java/cham/Lab4/JImageDisplay.java
+++ Lab4/src/main/java/cham/Lab4/JImageDisplay.java
@@ -9,23 +9,23 @@ import javax.swing.JComponent;
 public class JImageDisplay extends JComponent{
 
 	/**
-	 * Класс реализации компонента отрисовки фрактала в окне 
+	 * Класс реализации компонента отрисовки фрактала в окне
 	 */
-	
+
 	private BufferedImage buf_image;
-	
+
 	public JImageDisplay(int height, int width) {
 		// Создание буфера для отрисовки изображения
 		buf_image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		// Задание размера компоненту
 		this.setPreferredSize(new Dimension(width, height));
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		g.drawImage(buf_image, 0, 0, this.getWidth(), this.getHeight(), null);
 	}
-	
+
 	// Очистка окна изображения
 	public void clearImage() {
 		for(int y = 0; y < buf_image.getHeight(); y++) {
@@ -34,10 +34,10 @@ public class JImageDisplay extends JComponent{
 			}
 		}
 	}
-	
+
 	// Отрисовка каждого пикселя
 	public void drawPixel(int x, int y, int rgbColor) {
 		buf_image.setRGB(x, y, rgbColor);
 	}
-	
+
 }
